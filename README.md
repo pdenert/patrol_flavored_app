96e4013794fae96d0329c93500f3155713485773
pdenert <pdenert09@gmail.com>
2024-06-06 17:06:26 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# patrol_flavored_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/patrol_flavored_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.patrol_flavored_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
